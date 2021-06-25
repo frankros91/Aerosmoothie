e66a7c761cddb34784a62c61b6c0ba8c5cb4511b
Frank Rossi <FrankRossi@C02D2B74MD6M.fios-router.home>
2021-06-25 10:30:28 -0400

    some code cleanup

---
+++ src/components/Smoothie/Smoothie.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Smoothie = () =>
+    <img
+        alt="Smoothie"
+        style={{ maxHeight: '250px'}}
+        src="https://www.wellplated.com/wp-content/uploads/2020/01/Greek-yogurt-smoothie-peanut-butter.jpg" />
+
+export default Smoothie
